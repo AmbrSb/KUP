e75e3024ce77649f8958ad2a9ff8c45a69ea5776
Amin Saba <amn.brhm.sb@gmail.com>
2021-03-21 18:21:42 +0330

    Update copyright notice

--- kupdev/kup_dev.c
+++ kupdev/kup_dev.c
@@ -1,7 +1,7 @@
 /**
  * BSD 3-Clause License
  * 
- * Copyright (c) 2020, Amin Saba
+ * Copyright (c) 2020-2021, Amin Saba
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
